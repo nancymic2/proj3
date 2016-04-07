d1b6c0e274afcc93e5b899b641e5dbcf8d8a0d9b
Nancy Michell <nancy.ann.michell@hp.com>
2016-04-07 14:22:13 -0400

    all new

--- public/js/mystuck.js
+++ public/js/mystuck.js
@@ -2,8 +2,7 @@
 window.onload=function(){
 
 	sizeit();
-	//document.getElementById('hamburger').addEventListener('click', getmenu, false);
-	//moved event listener inline because pages are so big
+
 };
 
 window.onresize=function(){
@@ -13,10 +12,7 @@ window.onresize=function(){
 
 
 function sizeit() {
-// onload or on change check the width of the window and get from the data attribute in the image tag the appropriate image
-// on small screens, only the smallest image will download, followed by medium, then large
-// images on all pages are served based on this function. however the photo credit page gets a larger image on a small screen as it looks better in a single column
-//images names *Cred are the small version. medium is *Med
+// onload or on change check the width of the window and get rid of stupid sticky class
 
 
 
@@ -26,14 +22,10 @@ function sizeit() {
 		if (window.matchMedia('(max-width: 1000px)').matches) {
 			stuck[i].setAttribute('class', 'nothing');
 			
-
-		
+	
 		}
-
-		
     }
 
-    return;
 }
 
 //this function shows and hides the hamburger menu
