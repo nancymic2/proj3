eae7769f1b37afe43ef0c3b45580269fbc1d4063
Nancy Michell <nancy.ann.michell@hp.com>
2016-04-07 10:18:22 -0400

    all new

--- public/receipt.php
+++ public/receipt.php
@@ -1,4 +1,18 @@
-<?php
+<!doctype html>
+<html lang="en" dir="ltr">
+  <head>
+   <script src="js/myjs.js"></script>
+
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reading with Penny</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+
+  </head>
+<body>
+	<?php
 
 
 $name = $_POST['name'];
@@ -15,3 +29,10 @@ echo $order;
 
 
 ?>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="js/jquery.rwdImageMaps.min.js"></script>
+
+   <script src="js/myjs.js"></script>
+</body>
+</html>
