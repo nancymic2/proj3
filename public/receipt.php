61ea6ac13fc4b88eaa556e63f0f0fc82251c0124
Nancy Michell <nancy.ann.michell@hp.com>
2016-04-08 20:20:26 -0400

    order

--- public/receipt.php
+++ public/receipt.php
@@ -96,7 +96,12 @@ echo '<br>Please look over your order and call us with any necessary changes.';
 
 
 ?>
+<pre>
 
+
+
+
+</pre>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script src="js/jquery.rwdImageMaps.min.js"></script>
 
